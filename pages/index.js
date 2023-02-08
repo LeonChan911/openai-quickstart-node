65711872eee34a0ff9b5cac2fdb54c95f8d274e7
chenyin <yin.chen@ximalaya.com>
2023-02-08 15:25:14 +0800

    feat:优化

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from "next/head";
 import { useState } from "react";
+import Toast from "light-toast";
 import styles from "./index.module.css";
 
 export default function Home() {
   const [textInput, setTextInput] = useState("");
   const [result, setResult] = useState();
+  const [ loading, setLoading] = useState(false);
 
   const onSubmit = async (type) => {
+    setLoading(true);
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
@@ -18,6 +21,7 @@ export default function Home() {
 
       const data = await response.json();
       if (response.status !== 200) {
+        Toast.info("AI出错啦~ 请重新再试");
         throw (
           data.error ||
           new Error(`Request failed with status ${response.status}`)
@@ -25,6 +29,7 @@ export default function Home() {
       }
 
       setResult(data.result);
+      setLoading(false);
       console.log("输入:", textInput);
       console.log("ai算法:", data.result);
     } catch (error) {
@@ -43,6 +48,7 @@ export default function Home() {
       <main className={styles.main}>
         {/* <img src="/dog.png" className={styles.icon} /> */}
         <h3>Yin的ai许愿瓶</h3>
+        {loading && <p>AI正在计算中...</p>}
         <div className={styles.form}>
           <textarea
             type="text"
@@ -50,8 +56,8 @@ export default function Home() {
             value={textInput}
             onChange={(e) => setTextInput(e.target.value)}
           />
-          <button onClick={() => onSubmit("image")}>生成图片</button>
-          <button onClick={() => onSubmit("text")}>文本回答</button>
+          <button disabled={loading} onClick={() => onSubmit("image")}>生成图片</button>
+          <button disabled={loading}  onClick={() => onSubmit("text")}>文本回答</button>
         </div>
         {/* <div className={styles.result}>{result}</div> */}
         {result && result.startsWith("http") ? (
