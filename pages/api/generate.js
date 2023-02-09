baea968a8b7482ea68f34fb9ddbbef177f33f9c9
chenyin <yin.chen@ximalaya.com>
2023-02-09 18:13:15 +0800

    feat:文件

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -4,46 +4,7 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const UserList = {
-  root: {
-    username: "root",
-    password: "chenyin1991911",
-    times: 1000000000,
-  },
-  user1: {
-    username: "user1",
-    password: "5tfded",
-    times: 30,
-  },
-  user2: {
-    username: "user2",
-    password: "3dfef4",
-    times: 30,
-  },
-  user3: {
-    username: "user3",
-    password: "134ed4",
-    times: 30,
-  },
-  user4: {
-    username: "user4",
-    password: "dfe3we",
-    times: 30,
-  },
-  user5: {
-    username: "user5",
-    password: "90jjj",
-    times: 30,
-  },
-  user6: {
-    username: "user6",
-    password: "23d00f",
-    times: 30,
-  },
-};
-
 export default async function (req, res) {
-
     if (!configuration.apiKey) {
       res.status(500).json({
         error: {
